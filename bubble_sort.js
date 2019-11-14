4ba7fa6d29b3e501365f82a28ab9039e4c25e972
mohamedumer <53818424+mohamedumer@users.noreply.github.com>
2019-11-14 20:30:02 +0530

    Update bubble_sort.js

--- bubble_sort.js
+++ bubble_sort.js
@@ -1,6 +1,7 @@
 var a=[2,3,6,4,3,2,1,6,4,3,2];
-for(i=0;i<a.length;i++){
-    for(j=i;j<a.length;j++){
+var len=a.length;
+for(i=0; i<len; i++){
+    for(j=i; j<len; j++){
         if(a[i]>a[j+1]){
             b=a[i];
             a[i]=a[j+1];
